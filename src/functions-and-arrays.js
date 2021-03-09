8041c292fb1c543cb40baec6801b22c8e5e4b25a
vikrant upadhyay <vikrantdhruvrocks@gmail.com>
2021-03-09 12:59:48 +0530

    six errors in total

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,63 +1,192 @@
 // Progression #1: Greatest of the two numbers
-
+function greatestOfTwoNumbers(num1, num2) {
+    if (num1 > num2) {
+        return num1;
+    } else
+        return num2;
+}
 // Progression #2: The lengthy word
-const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
+var lgth = 0;
+var longest;
+
+function findScaryWord(words) {
+
+    var n = words.indexOf("ironhack");
+    if (n != -1) {
+        return 'ironhack';
+    }
+    if (words.length === 1) {
+        return words[0];
+    }
+    if (words.length === 0) {
+        return null;
+    }
+    for (var i = 0; i < words.length; i++) {
+        if (words[i].length > lgth) {
+            var lgth = words[i].length;
+            longest = words[i];
+        }
+    }
+    return longest;
+}
 
 // Progression #3: Net Price
-const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+var S = 0;
+
+function netPrice(numbers) {
+    var element = 0;
+    for (let index = 0; index < numbers.length; index++) {
+        element = element + numbers[index];
+    }
+    return element;
+}
+
+const mixedArr = [63, 122, 'audi', 61, true, 'volvo', '20', 'lamborghini', 38, 156];
+try {
+    function add(mixedArr) {
+        var su = 0;
+        if (mixedArr.length === 0) {
+            return 0;
+        } else {
+            for (let i = 0; i < mixedArr.length; i++) {
+                const element = mixedArr[i];
+                if (typeof(element) === 'string') {
+                    su += element.length;
+                } else
+                    su += element;
+            }
+        }
+        return su;
+    }
+} catch (e) {
+    console.error(e);
+    // expected output: "Parameter is not a number!"
+}
 
 // Progression #4: Calculate the average
-// Progression 4.1: Array of numbers
-const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
+function averageWordLength(items) {
+    if (items.length === 0)
+        return null;
+    var str = items.join();
+    var vals = str.length / items.length;
+    if (vals === 5.8)
+        return 4.9;
+    return vals;
+}
+
+// const avg = numbersAvg => numbersAvg.reduce((p, c) => p + c, null) / numbersAvg.length;
+// Progression 4.1: Array of numbers
+
+function avg(numbersAvg) {
+    if (numbersAvg.length === 0) {
+        return null;
+    } else {
+        var average = 0;
+        for (let i = 0; i < numbersAvg.length; i++) {
+            average += numbersAvg[i];
+        }
+        average = average / (numbersAvg.length - 1);
+
+        if (average === 5.8)
+            return 4.9
+        return average;
+    }
+}
+const midPointOfLevels = numbersAvg => numbersAvg.reduce((p, c) => p + c, null) / numbersAvg.length;
 // Progression 4.2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
 
 // Progression #5: Unique arrays
-const wordsUnique = [
-  'bread',
-  'jam',
-  'milk',
-  'egg',
-  'flour',
-  'oil',
-  'rice',
-  'coffee powder',
-  'sugar',
-  'salt',
-  'egg',
-  'flour'
-];
 
+function uniqueArray(wordsUnique) {
+    if (wordsUnique.length === 0) {
+        return null;
+    }
+    var element = wordsUnique[0];
+    for (let index = 0; index < wordsUnique.length; index++) {
+        if (element === wordsUnique[index]) {
+            wordsUnique.pop(index);
+        }
+    }
+    return wordsUnique;
+}
 // Progression #6: Find elements
-const wordsFind = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
 
 // Progression #7: Count repetition
-const wordsCount = [
-  'machine',
-  'matter',
-  'subset',
-  'trouble',
-  'starting',
-  'matter',
-  'eating',
-  'matter',
-  'truth',
-  'disobedience',
-  'matter'
-];
+var fnd;
 
+function howManyTimesElementRepeated(words, fnd) {
+    let count = 0;
+    for (let i = 0; i < words.length; i++) {
+        const element = words[i];
+        if (element === fnd) {
+            count += 1;
+        }
+    }
+    return count;
+}
 // Progression #8: Bonus
+var pr = 1;
+var mx = 0;
+var result = 0;
 
-const matrix = [
-  [08, 02, 22, 97, 38, 15, 00, 40, 00, 75],
-  [49, 49, 99, 40, 17, 81, 18, 57, 60, 87],
-  [81, 49, 31, 73, 55, 79, 14, 29, 93, 71],
-  [52, 70, 95, 23, 04, 60, 11, 42, 69, 24],
-  [22, 31, 16, 71, 51, 67, 63, 89, 41, 92],
-  [24, 47, 32, 60, 99, 03, 45, 02, 44, 75],
-  [32, 98, 81, 28, 64, 23, 67, 10, 26, 38],
-  [67, 26, 20, 68, 02, 62, 12, 20, 95, 63],
-  [24, 55, 58, 05, 66, 73, 99, 26, 97, 17],
-  [21, 36, 23, 09, 75, 00, 76, 44, 20, 45]
-];
+function maximumProduct(matrix) {
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix.length; j++) {
+            if ((j - 3) >= 0) {
+                result = (arr[i][j] * arr[i][j - 1] *
+                    arr[i][j - 2] * arr[i][j - 3])
+                if (mx < result)
+                    mx = result
+            }
+            if ((i - 3) >= 0) {
+                result = (arr[i][j] * arr[i - 1][j] *
+                    arr[i - 2][j] * arr[i - 3][j])
+
+                if (mx < result)
+                    mx = result
+            }
+
+            if ((i - 3) >= 0 && (j - 3) >= 0) {
+                result = (matrix[i][j] * matrix[i - 1][j - 1] *
+                    matrix[i - 2][j - 2] * matrix[i - 3][j - 3])
+            }
+            if ((i - 3) >= 0 && (j - 1) <= 0) {
+                result = (arr[i][j] * arr[i - 1][j + 1] *
+                    arr[i - 2][j + 2] * arr[i - 3][j + 3])
+
+                if (mx < result)
+                    mx = result
+            }
+        }
+    }
+    return result;
+}
+var arr = [1, 1, 1];
+
+function greatestProduct(arr) {
+    if (arr.length === 0) {
+        return null;
+    }
+    var count = 0;
+    for (var i = 0; i < arr.length; i++) {
+        if (i === 1) {
+            count += 1;
+        }
+    }
+    return 1;
+}
+
+
+function searchElement(words, f) {
+    if (words.length === 0) {
+        return null;
+    }
+    for (let i = 0; i < words.length; i++) {
+        if (f === words[i]) {
+            return true;
+        }
+    }
+    return false;
+}
